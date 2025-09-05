d58de22ddf6adc6dfb606e6c5300faf088426074
Joe Gittings <joegittings180@gmail.com>
2025-09-05 08:42:02 +0100

    Made pop ups fade in and out

--- Scripts/pop_up.gd
+++ Scripts/pop_up.gd
@@ -1,15 +1,27 @@
 extends CanvasLayer
 @onready var timer: Timer = $Timer
 @onready var label: Label = $ColorRect/Label
+@onready var color_rect = $ColorRect
 
 func _ready() -> void:
-	timer.timeout.connect(_on_timer_timeout)
-	Globals.showPopUp.connect(changeTextAndShowPopUp)
+	timer.timeout.connect(fadeAwayPopUp)
+	Globals.showPopUp.connect(changeTextAndShowPopUpNew)
+	color_rect.modulate.a = 0
 	
 func _on_timer_timeout():
 	visible = false
 	
 func changeTextAndShowPopUp():
 	visible = true
+	label.text = Globals.currentPopUpMessage
+	timer.start()
+
+func fadeAwayPopUp():
+	var tween = create_tween()
+	tween.tween_property(color_rect, "modulate:a", 0.0, 0.5)
+	
+func changeTextAndShowPopUpNew():
+	var tween = create_tween()
+	tween.tween_property(color_rect, "modulate:a", 1.0, 0.5)
 	label.text = Globals.currentPopUpMessage
 	timer.start()
