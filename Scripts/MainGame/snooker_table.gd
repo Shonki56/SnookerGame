ba3fc8ff91649572069355141ec67907dab1adf2
Joe Gittings <joegittings180@gmail.com>
2025-09-12 00:21:23 +0100

    Now uses new time system to payout for tables

--- Scripts/MainGame/snooker_table.gd
+++ Scripts/MainGame/snooker_table.gd
@@ -5,17 +5,9 @@ extends Node2D
 @export var tablePricePerHour: float
 @onready var time_system: TimeSystem = $TimeSystem
 
-
-func _ready() -> void:
-	pay_timer.timeout.connect(payForFullHour)
-	print(str(Globals.currentTime.minutes))
-
-
 var isTableBeingUsed = false
 
 var currentSessionPrice = 0
-
-var payTimerHourInSeconds = 10.0
 
 var tableQualityString: String
 
@@ -25,51 +17,33 @@ var totalTimeTableWasOn
 
 func setTimeOnStart():
 	timeOnStart = Globals.currentTime.duplicate()
-	print_debug(str(timeOnStart.hours) + ":" + str(timeOnStart.minutes))
 	
 func setTimeEnd():
 	timeOnEnd = Globals.currentTime.duplicate()
-	print_debug(str(timeOnEnd.hours) + ":" + str(timeOnEnd.minutes))
-	
-func calculateTotalTimeTableWasOn():
-	var totalHours = timeOnEnd.hours - timeOnStart.hours
-	var totalMinutes = timeOnEnd.minutes - timeOnStart.minutes
-	print("Total minutes table was on:" + str(totalMinutes))
 
 func turnTableOnOrOff():
 	if color_rect.color == Color.WHITE:
 		color_rect.color = Color.GREEN
 		setTimeOnStart()
+		isTableBeingUsed = true
 	else:
+		isTableBeingUsed = false
 		color_rect.color = Color.WHITE
 		isTableBeingUsed = !isTableBeingUsed
-		pauseOrPlayTimerAndPayMoney(pay_timer)
 		setTimeEnd()
-		calculateTotalTimeTableWasOn()
+		calculatePriceForTableUse()
+		addMoneyAndResetCurrentSessionAmount()
 		
 func calculatePriceForTableUse():
-	#TODO Do this
-	pass
+	var totalHours = timeOnEnd.hours - timeOnStart.hours
+	var totalMinutes = timeOnEnd.minutes - timeOnStart.minutes 
+	var hoursPrice = totalHours * tablePricePerHour
+	var minutesPrice = (totalMinutes * tablePricePerHour) / 60
+	currentSessionPrice = hoursPrice + minutesPrice 
+	
+func addMoneyAndResetCurrentSessionAmount():
+	Globals.total_money += currentSessionPrice
+	currentSessionPrice = 0
 	
 func payForFullHour():
 	currentSessionPrice += tablePricePerHour
-	
-func payForIncompleteHour():
-	var incompleteTableMultiplier = tablePricePerHour / payTimerHourInSeconds
-	var timeToChargeFor = pay_timer.wait_time - pay_timer.time_left
-	var priceToCharge = timeToChargeFor * incompleteTableMultiplier
-	currentSessionPrice += priceToCharge
-	
-func pauseOrPlayTimerAndPayMoney(timer: Timer):
-	if timer.is_stopped() == true or timer.paused == true:
-		timer.paused = false
-		timer.start()
-		print(str(tablePricePerHour))
-	elif timer.paused == false:
-		timer.paused = true
-		payForIncompleteHour()
-		Globals.total_money += currentSessionPrice
-		
-		currentSessionPrice = 0
-	
-	
